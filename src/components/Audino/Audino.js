2e02e53e98e4b88acc5097d848b70323373f71f9
kafkaesc <allthosemoments@gmail.com>
2018-05-17 12:38:38 -0500

    filled out createPet, used ternary to rm form and display the pet

--- src/components/Audino/Audino.js
+++ src/components/Audino/Audino.js
@@ -6,7 +6,8 @@ class Audino extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {name: 'Nonam', species: 'Ditto', happiness: 5, hunger: 5};
+    this.state = {name: 'Nonam', species: 'Ditto', happiness: 0, hunger: 0};
+    this.state = {petCreated: false};
 
     this.createPet = this.createPet.bind(this);
   }
@@ -14,20 +15,26 @@ class Audino extends React.Component {
   createPet(givenName, givenSpecies) {
     this.setState({
       name: givenName,
-      species: givenSpecies
+      species: givenSpecies,
+      happiness: 5,
+      hunger: 5,
+      petCreated: true
     });
   }
 
   render() {
+    const petDisplay = this.state.petCreated;
+    const appContent = petDisplay
+      ? (<Pet
+          name={this.state.name}
+          species={this.state.species}
+          happiness={this.state.happiness}
+          hunger={this.state.hunger} />)
+      : (<PetCreator
+        onSubmit={this.createPet} />);
     return (
       <div>
-          <PetCreator
-            onSubmit={this.createPet} />
-          <Pet
-            name={this.state.name}
-            species={this.state.species}
-            happiness={this.state.happiness}
-            hunger={this.state.hunger} />
+        {appContent}
       </div>
     );
   }
