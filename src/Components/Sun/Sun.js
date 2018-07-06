1454392971eddc308fd682ae2917750b98e9b09b
kafkaesc <allthosemoments@gmail.com>
2018-07-06 02:07:47 -0500

    rm old comment

--- src/Components/Sun/Sun.js
+++ src/Components/Sun/Sun.js
@@ -24,8 +24,6 @@ class Sun extends React.Component {
     }
   }
 
-  // bamboolze, literally no code is written to move
-  // to then next cycle of stellar existence yet
   printNextPhase() {
     switch(this.state.phase) {
       case 'Molecular Cloud':
